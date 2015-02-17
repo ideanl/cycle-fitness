2e646ad793b35fe61f5ced565b2c751f4a0ecdcf
Idean Labib <idean1.labib@gmail.com>
2015-02-17 11:14:25 -0800

    fixed fading for slideshow

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,23 +2,18 @@
 $(document).on('page:change', function() {
   index_max = $('.slideshow-img').length - 1
   index = index_max;
-  last_index = 0;
   
-  $('[id^=image]').hide();
-  $('#image' + last_index).show();
-
   // Switch image every five seconds creating slideshow.
   clearInterval(window.i);
 
   window.i = window.setInterval(function(){
-    console.log("HEREEEE");
-    $('#image' + last_index).toggle();
+    if (index > index_max)
+      $('[id^=image]').show();
 
     $('#image' + index).fadeToggle();
 
-    last_index = index;
-    if(index-- == 0) {
-      index = index_max;
-    }
-  },6000);
+    if (index-- == 1)
+      index = index_max + 1;
+
+  }, 6000);
 });
